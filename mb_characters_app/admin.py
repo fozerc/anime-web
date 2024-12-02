d45ad59626d92e3d9c01d5d822c59fd3b6108b67
fozerc <an07ka@gmail.com>
2024-12-02 13:13:55 +0100

    admin

--- mb_characters_app/admin.py
+++ mb_characters_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from mb_characters_app.models import AnimeUser, CharacterModel, MangaModel, PostModel, SectionModel, WikiModel
+
+admin.site.register(AnimeUser)
+admin.site.register(CharacterModel)
+admin.site.register(MangaModel)
+admin.site.register(PostModel)
+admin.site.register(SectionModel)
+admin.site.register(WikiModel)
